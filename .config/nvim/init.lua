2ada41e56825f76570dab33f5969890e692728b7
cf913 <chaddudz@hotmail.fr>
2024-07-07 13:02:29 +1000

    spectre config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,7 +31,9 @@ require("lazy").setup({
       opts = { open_cmd = "noswapfile vnew" },
       -- stylua: ignore
       keys = {
-        { "<leader>sr", function() require("spectre").toggle() end, desc = "Replace in Files (Spectre)" },
+        { "<leader>sr", function() require("spectre").toggle() end,                                 desc = "Replace in Files (Spectre)" },
+        { "<leader>sw", function() require("spectre").open_visual({ select_word = true }) end,      desc = "Replace Selected Word (Spectre)" },
+        { "<leader>sf", function() require("spectre").open_file_search({ select_word = true }) end, desc = "Replace in Current File (Spectre)" },
       },
     },
     {
