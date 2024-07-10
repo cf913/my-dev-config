e0a199feaa3cc93b348539c6b1a86acf627b984c
cf913 <chaddudz@hotmail.fr>
2024-07-10 13:32:52 +1000

    move line keybinds + colorizer with tailwindcss

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "       -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
+
 require("lazy").setup({
   spec = {
     { import = "plugins" },
@@ -110,8 +111,34 @@ require("lazy").setup({
       "karb94/neoscroll.nvim"
     },
     {
-      'norcalli/nvim-colorizer.lua',
-      opts = {}
+      'NvChad/nvim-colorizer.lua',
+      opts = {
+        filetypes = { "*" },
+        user_default_options = {
+          RGB = true,          -- #RGB hex codes
+          RRGGBB = true,       -- #RRGGBB hex codes
+          names = true,        -- "Name" codes like Blue or blue
+          RRGGBBAA = true,     -- #RRGGBBAA hex codes
+          AARRGGBB = false,    -- 0xAARRGGBB hex codes
+          rgb_fn = false,      -- CSS rgb() and rgba() functions
+          hsl_fn = false,      -- CSS hsl() and hsla() functions
+          css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = false,      -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          -- Available modes for `mode`: foreground, background,  virtualtext
+          mode = "background", -- Set the display mode.
+          -- Available methods are false / true / "normal" / "lsp" / "both"
+          -- True is same as normal
+          tailwind = true,                                 -- Enable tailwind colors
+          -- parsers can contain values used in |user_default_options|
+          sass = { enable = false, parsers = { "css" }, }, -- Enable sass colors
+          virtualtext = "■",
+          -- update color values even if buffer is not focused
+          -- example use: cmp_menu, cmp_docs
+          always_update = false
+        },
+        -- all the sub-options of filetypes apply to buftypes
+        buftypes = {},
+      },
     },
     {
       'lewis6991/gitsigns.nvim',
--- .config/nvim/lua/cf913/init.lua
+++ .config/nvim/lua/cf913/init.lua
@@ -12,6 +12,7 @@ vim.o.termguicolors = true  -- Enable 24-bit RGB colors
 vim.o.conceallevel = 2
 -- vim.opt.mouse = ""
 
+-- NEOVIDE ONLY CONFIG
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   -- Helper function for transparency formatting
@@ -21,48 +22,28 @@ if vim.g.neovide then
   vim.g.neovide_padding_left = 0
 
   vim.g.neovide_scale_factor = 1.0
-
-  -- Helper function for transparency formatting
-  -- local alpha = function()
-  --   return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-  -- end
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  -- vim.g.neovide_transparency = 0.75
-  -- vim.g.transparency = 0
-  -- vim.g.neovide_background_color = "#0f1117" .. alpha()
-  --
-  -- vim.g.neovide_window_blurred = true
-  -- vim.g.neovide_floating_blur_amount_x = 2.0
-  -- vim.g.neovide_floating_blur_amount_y = 2.0
-
   vim.g.neovide_hide_mouse_when_typing = true
 
-  vim.g.neovide_scroll_animation_length = 0.15
+  vim.g.neovide_scroll_animation_length = 0.2
   vim.g.neovide_refresh_rate = 120
   vim.g.neovide_confirm_quit = true
   vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_animate_in_insert_mode = true
   -- not sure if i like smooth inserts or not. WIll have to test both.
-end
-
-if vim.g.neovide then
+  vim.keymap.set({ 'n', 'i', 'v' }, '<D-k>', 'ddkP', { desc = 'Move line UP' })
+  vim.keymap.set({ 'n', 'i', 'v' }, '<D-j>', 'ddp', { desc = 'Move line DOWN' })
+  vim.keymap.set('n', '<D-s>', vim.cmd.w)     -- Save
   vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
   vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
   vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
   vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
   vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
-end
-
-if vim.g.neovide == true then
   vim.api.nvim_set_keymap("n", "<D-=>",
     ":lua vim.g.neovide_scale_factor = math.min(vim.g.neovide_scale_factor + 0.1,  2.0)<CR>", { silent = true })
   vim.api.nvim_set_keymap("n", "<D-->",
     ":lua vim.g.neovide_scale_factor = math.max(vim.g.neovide_scale_factor - 0.1,  0.1)<CR>", { silent = true })
 end
 
-vim.keymap.set({ 'n', 'i', 'v' }, '<D-k>', 'ddkP', { desc = 'Move line UP' })
-vim.keymap.set({ 'n', 'i', 'v' }, '<D-j>', 'ddp', { desc = 'Move line DOWN' })
-vim.keymap.set('n', '<D-s>', vim.cmd.w) -- Save
 
 -- Allow clipboard copy paste in neovim
 vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
