9538965bbf1b1cad03675ec6755e64eb0ca6b997
cf913 <chaddudz@hotmail.fr>
2024-07-14 16:35:45 +1000

    fix c-; not working + alacritty padding per setup (manual)

--- .config/nvim/plugin/harpoon.lua
+++ .config/nvim/plugin/harpoon.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon
 vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end, { desc = "Harpoon: Buffer 1" })
 vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end, { desc = "Harpoon: Buffer 2" })
 vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end, { desc = "Harpoon: Buffer 3" })
-vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end, { desc = "Harpoon: Buffer 4" })
+vim.keymap.set("n", ";", function() harpoon:list():select(4) end, { desc = "Harpoon: Buffer 4" })
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Harpoon: Buffer PREV" })
