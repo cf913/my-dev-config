6a30dee15bb9a9ed1834a14f2c9a9cbb91d0905f
cf913 <chaddudz@hotmail.fr>
2024-07-27 21:06:26 +1000

    Telescope dropdown theme

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,15 +3,21 @@ return {
   tag = '0.1.8',
   lazy = true,
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require('telescope').setup({
-      defaults = {
-        layout_config = {
-          height = 0.9,
-          width = 0.9,
-          preview_width = 0.5,
-        },
-      },
-    })
-  end
+  opts = {
+    defaults = {
+      -- layout_strategy = 'dropdown',
+      -- layout_config = {
+      --   height = 0.9,
+      --   width = 0.5,
+      --   prompt_position = 'top',
+      -- },
+      -- sorting_strategy = "ascending"
+    },
+    pickers = {
+      find_files = {
+        theme = "dropdown",
+      }
+    },
+  }
+
 }
