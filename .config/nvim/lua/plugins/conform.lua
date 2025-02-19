8bc7ee931a51b5d64dd0d51dbccb2e4160163daa
cf913 <chaddudz@hotmail.fr>
2025-02-19 16:14:55 +1000

    fix: format on save not triggering

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,12 +2,17 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   opts = {
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
     formatters_by_ft = {
       -- Use a sub-list to run only the first available formatter
       -- lua = { 'stylua' },
+      typescriptreact = { "prettier", "prettierd", stop_after_first = true },
+      typescript = { "prettier", "prettierd", stop_after_first = true },
       javascript = { "prettier", stop_after_first = true },
-      typescript = { "prettier", stop_after_first = true },
-      typescriptreact = { "prettier", stop_after_first = true },
       css = { "prettier", stop_after_first = true },
     },
   },
